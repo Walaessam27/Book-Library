78d750ac724984b06aa864cf132e98370612f6ea
sadeen_hawash <sadeenghawash593@gmail.com>
2024-10-29 19:18:07 +0200

    home page

--- js/home.js
+++ js/home.js
@@ -14,3 +14,42 @@ let swiperHome = new Swiper(".home-swiper", {
     },
   },
 });
+
+const swiper = new Swiper(".saved-books", {
+  slidesPerView: 1.2, // Number of slides per view
+  spaceBetween: 20, // Space between slides
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    390: {
+      slidesPerView: 1.5,
+      spaceBetween: 15,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    1441: {
+      slidesPerView: 6,
+      spaceBetween: 25,
+    },
+    1900: {
+      slidesPerView: 7,
+      spaceBetween: 25,
+    },
+  },
+});
