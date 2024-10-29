5b5114ff2029f757d4f2ff1713454ae7dd8f98c3
sadeen_hawash <sadeenghawash593@gmail.com>
2024-10-29 21:38:38 +0200

    home page trending sec

--- js/home.js
+++ js/home.js
@@ -15,7 +15,7 @@ let swiperHome = new Swiper(".home-swiper", {
   },
 });
 
-const swiper = new Swiper(".saved-books", {
+const saved_swiper = new Swiper(".saved-books", {
   slidesPerView: 1.2, // Number of slides per view
   spaceBetween: 20, // Space between slides
   navigation: {
@@ -49,6 +49,45 @@ const swiper = new Swiper(".saved-books", {
     },
     1900: {
       slidesPerView: 7,
+      spaceBetween: 25,
+    },
+  },
+});
+
+const news_swiper = new Swiper(".news-books", {
+  slidesPerView: 1.2, // Number of slides per view
+  spaceBetween: 20, // Space between slides
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    390: {
+      slidesPerView: 1.5,
+      spaceBetween: 15,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    1441: {
+      slidesPerView: 5,
+      spaceBetween: 25,
+    },
+    1900: {
+      slidesPerView: 5,
       spaceBetween: 25,
     },
   },
