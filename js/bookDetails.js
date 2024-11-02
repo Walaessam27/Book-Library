677c9e37a184bf56f543e4648e1fc207643b78d8
sadeen_hawash <sadeenghawash593@gmail.com>
2024-11-02 14:24:29 +0200

    book details page swipers

--- js/bookDetails.js
+++ js/bookDetails.js
@@ -63,3 +63,77 @@ const news_swiper = new Swiper(".news-books", {
     },
   },
 });
+const articles_swiper = new Swiper(".articles-swiper", {
+  slidesPerView: 1, // Number of slides per view
+  spaceBetween: 20, // Space between slides
+  navigation: {
+    nextEl: ".articles-swiper .swiper-button-next",
+    prevEl: ".articles-swiper .swiper-button-prev",
+  },
+  breakpoints: {
+    390: {
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1441: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+    },
+    1900: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+    },
+  },
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const bookDetails = document.getElementById("bookDetails");
+  const fixedSection = document.getElementById("fix");
+
+  if (!bookDetails || !fixedSection) {
+    console.error("Could not find elements with IDs 'bookDetails' or 'fix'.");
+    return;
+  }
+  const bookDetailsHeight = bookDetails.offsetHeight;
+  const minWidth = window.matchMedia("(min-width: 1024px)");
+
+  const handleScroll = () => {
+    if (window.scrollY >= (bookDetailsHeight * 3) / 5) {
+      console.log(
+        "Book Details Height 3/5 reached: " +
+          window.scrollY +
+          ", " +
+          (bookDetailsHeight * 3) / 5
+      );
+      fixedSection.style.position = "absolute";
+    } else {
+      fixedSection.style.position = "fixed";
+    }
+  };
+
+  // Check if min-width condition is met and add/remove scroll event accordingly
+  const handleResize = () => {
+    if (minWidth.matches) {
+      document.addEventListener("scroll", handleScroll);
+    } else {
+      document.removeEventListener("scroll", handleScroll);
+      // Reset position style when width is below 1024px
+      fixedSection.style.position = "";
+    }
+  };
+
+  // Initial check and setup for resize event
+  handleResize();
+  minWidth.addEventListener("change", handleResize);
+});
