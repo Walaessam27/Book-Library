f69ac487dd9568596ece4b0ecf381cbbff672734
Walaessam27 <145562141+Walaessam27@users.noreply.github.com>
2024-11-03 15:25:33 +0200

    sign commit

---
+++ My own/Book-Library-f88899d438c9906b67cf6f2df54bb1a5f6cc1e2c/bookDetails.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('.rating-system i').forEach((star, index) => {
+  star.addEventListener('click', () => {
+    const ratingValue = index + 1;
+    document.getElementById('rating-value').textContent = `${ratingValue}/5`;
+  });
+});
+
+document.getElementById('wishlist-btn').addEventListener('click', () => {
+  const wishlistButton = document.getElementById('wishlist-btn');
+  wishlistButton.classList.toggle('active');
+  if (wishlistButton.classList.contains('active')) {
+    wishlistButton.textContent = 'Added to Wishlist!';
+  } else {
+    wishlistButton.textContent = 'Add to Wishlist';
+  }
+});
+
+document.getElementById('read-btn').addEventListener('click', () => {
+  const readButton = document.getElementById('read-btn');
+  readButton.classList.toggle('active');
+  if (readButton.classList.contains('active')) {
+    readButton.textContent = 'Marked as Read!';
+  } else {
+    readButton.textContent = 'Mark as Read';
+  }
+});
